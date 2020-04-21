ecb73fdbce054003600cdf005bc7ee65f04340cd
Jeff Maughan <geophray@gmail.com>
2020-04-21 15:50:25 -0700

    Add ajax post request to App.search

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,22 +7,30 @@ import RepoList from './components/RepoList.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       repos: []
     }
+  }
 
+  // Load top 25 repos in database when component mounts
+  componentDidMount () {
+    $.get('/repos', this.render);
   }
 
   search (term) {
-    console.log(`${term} was searched`);
-    // TODO
+    $.post('/repos', {"username": term})
+      .done((data) => console.log('Saved.', data))
+      .fail((err) => {
+        console.error(err);
+        alert(`Lookup for user ${term} failed. Please try again.`)
+      });
   }
 
   render () {
     return (<div>
       <h1>Github Fetcher</h1>
-      <RepoList repos={this.state.repos}/>
-      <Search onSearch={this.search.bind(this)}/>
+      <RepoList repos={this.state.repos} />
+      <Search onSearch={this.search.bind(this)} />
     </div>)
   }
 }
